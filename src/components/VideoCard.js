b348eb2faa15c8ec456a1a0fa2217fd947612359
ahan-stark <ahanabubakar@gmail.com>
2024-08-27 23:18:28 +0530

    changed css alignment in homepage

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -4,10 +4,12 @@ const VideoCard = (props) => {
   const { snippet, statistics } = props.info;
   const { channelTitle, title, thumbnails } = snippet;
   return (
-    <div className="w-72 shadow-lg p-2 m-22 h-[18em] hover:scale-105">
+    <div className="w-72 shadow-lg p-2 m-22 h-[100%] hover:scale-105">
       <img className="rounded-lg" src={thumbnails.medium.url} alt="☹️" />
       <ul>
-        <li className="font-bold" >{title}</li>
+        <div className="">
+        <li className="font-semibold overflow-hidden" >{title}</li>
+        </div>
         <li className="text-red-500 font-semibold">{channelTitle}</li>
         <li>{statistics?.viewCount}</li>
       </ul>
